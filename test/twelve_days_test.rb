041e339ac7fe98be04ca47f9034c8e75013be46d
Mariusz Rzeppa <mrzeppa16@gmail.com>
2019-10-28 13:13:40 +0100

    xd

--- test/twelve_days_test.rb
+++ test/twelve_days_test.rb
@@ -3,7 +3,7 @@ require_relative '../lib/twelve_days'
 
 class TwelveDaysTest < Minitest::Test
   def test_the_whole_song
-    song_file = File.expand_path('../test/song.txt', __FILE__)
+    song_file = File.expand_path('song.txt', __FILE__)
     expected = IO.read(song_file)
     assert_equal expected, TwelveDays.song
   end
