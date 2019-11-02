a5b22a9993610498cd64d35ca64e2faf075cd09c
mrzeppa <mrzeppa16@gmail.com>
2019-11-03 00:11:50 +0100

    hamming

--- spec/hamming_spec.rb
+++ spec/hamming_spec.rb
@@ -4,5 +4,24 @@ describe Hamming do
 		expect(Hamming.compute('', '')).to eq 0
 	end
 	
+	it "test_single_letter_identical_strands" do
+		expect(Hamming.compute('A', 'A')).to eq 0
+	end
 	
+	it "test_single_letter_different_strands" do
+		expect(Hamming.compute('G', 'T')).to eq 1
+	end
+	
+	it "test_long_identical_strands" do
+		expect(Hamming.compute('GGACTGAAATCTG', 'GGACTGAAATCTG')).to eq 0
+	end
+	it "test_long_different_strands" do
+		expect(Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')).to eq 9
+	end
+	it "test_disallow_first_strand_longer" do
+		expect { Hamming.compute('AATG', 'AAA') }.to raise_error(ArgumentError)
+	end
+	it "test_disallow_second_strand_longer" do
+		expect { Hamming.compute('ATA', 'AGTG') }.to raise_error(ArgumentError)
+	end
 end
