f18a6863f5f39d387023ce838d5b11e23b3a2cb9
mrzeppa <mrzeppa16@gmail.com>
2019-11-02 17:33:57 +0100

    two fer

---
+++ spec/two_fer_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/two_fer'
+describe TwoFer do
+	it "Case 1" do
+		expect(TwoFer.two_fer).to eq "One for you, one for me."
+	end
+	it "Case 2" do
+		expect(TwoFer.two_fer).to eq "One for Alice, one for me."
+	end
+	it "Case 2" do
+		expect(TwoFer.two_fer).to eq "One for Bob, one for me."
+  end
+end
