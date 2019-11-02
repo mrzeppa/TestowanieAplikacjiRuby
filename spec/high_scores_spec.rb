21e395272c62a21a1aeeca6716fe8fc82bdd17d2
mrzeppa <mrzeppa16@gmail.com>
2019-11-02 18:59:44 +0100

    high scores

--- spec/high_scores_spec.rb
+++ spec/high_scores_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/high_scores'
 describe HighScores do
 	it "test_list_of_scores" do
+		scores = [30, 50, 20, 70]
 		expect(HighScores.new(scores).scores).to eq [30, 50, 20, 70]
 	end
 	
