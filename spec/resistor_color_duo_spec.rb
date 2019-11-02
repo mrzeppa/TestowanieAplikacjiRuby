5542291a96b1cbc42ebeb271193847e90ad613db
mrzeppa <mrzeppa16@gmail.com>
2019-11-02 17:54:55 +0100

    resistor colors

--- spec/resistor_color_duo_spec.rb
+++ spec/resistor_color_duo_spec.rb
@@ -1,12 +1,18 @@
 require_relative '../lib/resistor_color_duo'
 describe TwoFer do
-	it "Case 1" do
-		expect(ResistorColorDuo.two_fer).to eq "One for you, one for me."
+	it "test_brown_and_black" do
+		expect(ResistorColorDuo.value(["brown", "black"])).to eq 10
 	end
-	it "Case 2" do
-		expect(TwoFer.two_fer).to eq "One for Alice, one for me."
+	it "test_blue_and_grey" do
+		expect(ResistorColorDuo.value(["blue", "grey"])).to eq 68
 	end
-	it "Case 2" do
-		expect(TwoFer.two_fer).to eq "One for Bob, one for me."
+	it "test_yellow_and_violet" do
+		expect(ResistorColorDuo.value(["yellow", "violet"])).to eq 47
+	end
+	it "test_orange_and_orange" do
+		expect(ResistorColorDuo.value(["orange", "orange"])).to eq 33
+	end
+	it "test_ignore_additional_colors" do
+		expect(ResistorColorDuo.value(["green", "brown", "orange"])).to eq 51
   end
 end
