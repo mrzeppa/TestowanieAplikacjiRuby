ffc06ddaa0f90d3526dc1b3c3e7f4e70a42a63ec
mrzeppa <mrzeppa16@gmail.com>
2019-11-03 14:06:14 +0100

    anagram

--- spec/allergies_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/allergies'
-describe Grains do
-	it "test_no_allergies_means_not_allergic" do
-		allergies = Allergies.new(0)
-		expect(allergies.allergic_to?('peanuts')).to eq false
-		expect(allergies.allergic_to?('cats')).to eq false
-		expect(allergies.allergic_to?('strawberries')).to eq false
-	end
-end
