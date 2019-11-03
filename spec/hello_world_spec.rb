2626e5acdb6c48e32dc862a8819df9e3a4f65a40
mrzeppa <mrzeppa16@gmail.com>
2019-11-03 16:45:05 +0100

    test

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/hello_world'
 describe HelloWorld do
   it "say hello" do
-    expect(HelloWorld.hello).to eq "Hello, World!"
+    expect(HelloWorld.hello).to eq "Hello, Waorld!"
   end
 end
